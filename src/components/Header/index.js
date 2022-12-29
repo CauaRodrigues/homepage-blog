63138b946efb21847ca6ee3056a0997ddbb5d980
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-12-29 19:02:54 -0300

    header

---
+++ src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<header>
+			<span className="legend">Our blog</span>
+			<h1>Stories and interviews</h1>
+			<p>
+				Subscribe to learn about new product features, the latest in technology,
+				solutions, and updates.
+			</p>
+
+			<form>
+				<div className="input-btn">
+					<input type="email" placeholder="Enter your email" />
+					<button>Subscribe</button>
+				</div>
+
+				<p>
+					We care about your data in our <span>privacy policy</span>
+				</p>
+			</form>
+		</header>
+	);
+};
+
+export default Header;
